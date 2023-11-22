8ad37b0e3a6c53c52a6ba73aa8f1139996a17759
cebrailkeskindag <cebrailkeskindag1549@gmail.com>
2023-11-22 22:57:23 +0300

    temalar kısmen uygulandı

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
-import 'package:expenseapp/pages/expense_list.dart';
 import 'package:expenseapp/pages/main_page.dart';
 import 'package:flutter/material.dart';
 
 // Single Responsibility
+ColorScheme lightColorScheme = ColorScheme.fromSeed(
+  seedColor: Colors.grey,
+  primary: Colors.greenAccent,
+  secondary: Colors.black,
+);
 void main() {
   runApp(
-    MaterialApp(theme: ThemeData(useMaterial3: true), home: MainPage()),
+    MaterialApp(
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: lightColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: lightColorScheme.primary,
+              foregroundColor: lightColorScheme.secondary),
+          cardTheme:
+              const CardTheme().copyWith(color: lightColorScheme.primary),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+        ),
+        home: MainPage()),
   );
 }
 
